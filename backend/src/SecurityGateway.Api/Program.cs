3529a12d876f8604f8bae13e57e0501ec9ee5a06
kamil0103 <kamilafzal147@hotmail.com>
2026-08-31 12:05:59 -0700

    fix(api): add JsonStringEnumConverter for enum model binding

--- backend/src/SecurityGateway.Api/Program.cs
+++ backend/src/SecurityGateway.Api/Program.cs
@@ -276,7 +276,8 @@ builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtB
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
